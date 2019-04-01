f899ea0d74c3aa0e479e415cb93cdac2822e3bcd
Logan464 <Thundercats464>
2019-03-31 21:08:30 -0600

    little by little

--- pokecards.js
+++ pokecards.js
@@ -1,14 +1,19 @@
-import { pokemon } = [
-console.log(pokemon)
+import { pokemon } from  '../data/pokemon.js'
 
+class Pokemon {
+consturctor(id) {
+this.id = id
+}
+}
 
-
-const mainContainer = document.querySelector('.container')
+const mainContainer =document.querySelector('.container')
 
 function createPokeCard(pokeData){
 let card = document.createElement('div')
 card.className ='box'
-let title = document.createElement('h2')
+let figure = document.createElement('figure')
+let caption = document.createElement('figcaption')
+let image = document.createElement('img')
 
 let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
 title.textContent = pokeData.name
