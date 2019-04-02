a9a007eb8170b4a1e5fa683c81617bbdde15d9f9
Logan464 <Thundercats464>
2019-04-02 13:21:58 -0600

    Adding more code and getting the basic cards to work

--- pokecards.js
+++ pokecards.js
@@ -28,12 +28,40 @@ card.appendChild(figure)
 mainContainer.appendChild(card)
 }
 
-pokemon.forEach((singleMon) => {
-    fetch(singleMon.url)
-    .then(function(response) {
-    return response.json();
-    })
-    .then(function(myJson) {
-    createPokeCard(myJson)
-    })
-)}
+//pokemon.forEach((singleMon) => {
+//    fetch(singleMon.url)
+ //   .then(function(response) {
+//    return response.json();
+//    })
+//    .then(function(myJson) {
+//    createPokeCard(myJson)
+//    })
+// )}
+
+function fetchSinglePokemon(id) {
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+.then(function(response) {
+return response.json()
+}
+})
+.then(function(retrievedPokemon) {
+console.log(typeof(retrievedPokemon.id))
+if(retrievedPokemon.id < 10) {
+retrievedPokemon.imageID = "00" + retrievedPokemon.id
+}
+if(retrievedPokemon.id > 9 && retrievedPokemon.id < 100 ) {
+retrievedPokemon.imageID = "0" + retrievedPokemon.id
+}
+if(retrievedPokemon.id > 99) {
+retrievedPokemon.imageID = retrievedPokemon.id
+}
+retrievedPokemon.name =retrievedPokemon.name.charAt(0).toUpperCase() + retrievedPokemon.name.slice(1) createPokeCard(retrievedPokemon)
+})
+}
+
+const newPokemonButton = document.querySelector('button')
+
+newPokemonButton.addEventListener('click', function() {
+let pokemonID =prompt('Enter an ID of an existing pokemon:')
+fetchSinglePokemon(pokemonID)
+});
