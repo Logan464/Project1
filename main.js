724e8ceabdc5dc9e6810ae2eaa99fe59a932ff59
Logan464 <48307320+Logan464@users.noreply.github.com>
2019-04-16 11:58:54 -0600

    Update main.js
    Final JS

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-import { people } from '/data/people.js'
-import { planets } from '/data/planets.js'
-
-const allHomeWorlds = people.map(person => {
-  let foundWorld = planets.find(planet => {
-    return planet.url === person.homeworld
-  })
-  return {
-    name: person.name,
-    home: foundWorld.name,
-    eye_color: person.eye_color,
-  }
-})
-
-console.log(allHomeWorlds)
-
-const mainContainer = document.createElement('div')
-mainContainer.className = 'container'
-
-allHomeWorlds.forEach(person => {
-  let personElement = document.createElement('div')
-  let planetElement = document.createElement('p')
-
-  personElement.className = 'box'
-  personElement.textContent = person.name
-  personElement.textContent = person.home
-  personElement.style.backgroundColor = person.eye_color
-
-  personElement.appendChild(planetElement)
-  mainContainer.appendChild(personElement)
-})
-
-document.body.appendChild(mainContainer)
+import { films } from './films.js'
+let titleElement = document.querySelector('title')
+let crawlElement = document.querySelector('crawl')
