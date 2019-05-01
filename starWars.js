02112b1307f5a36d92f449e4dc0446df1f40e3ae
Logan464 <48307320+Logan464@users.noreply.github.com>
2019-04-30 21:48:57 -0600

    Update starWars.js
    js

--- starWars.js
+++ starWars.js
@@ -1,15 +1,30 @@
-import { people } from '/data/people.js'
-import { planets } from '/data/planets.js'
+import { people } from '../data/people.js'
+import { planets } from '../data/planets.js'
+
+const intro = document.querySelector('.intro')
+
+const getLastNumber = (url) => {
+    let end = url.lastIndexOf('/')
+    let start = end -2
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
 
 const allHomeWorlds = people.map(person => {
-  let foundWorld = planets.find(planet => {
-    return planet.url === person.homeworld
-  })
-  return {
-    name: person.name,
-    home: foundWorld.name,
-    eye_color: person.eye_color,
-  }
+    let foundWorld = planets.find(planet => {
+        return planet.url === person.homeworld
+    })
+
+    let imageURL = getLastNumber(person.url)
+    return {
+        name: person.name,
+        home: foundWorld.name,
+        eye_color: person.eye_color,
+        personImagePath: `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`
+    }
 })
 
 console.log(allHomeWorlds)
@@ -17,17 +32,27 @@ console.log(allHomeWorlds)
 const mainContainer = document.createElement('div')
 mainContainer.className = 'container'
 
-allHomeWorlds.forEach(person => {
-  let personElement = document.createElement('div')
-  let planetElement = document.createElement('p')
+allHomeWorlds.forEach((person) => {
+    let personElement = document.createElement('div')
+    personElement.className = 'box'
 
-  personElement.className = 'box'
-  personElement.textContent = person.name
-  personElement.textContent = person.home
-  personElement.style.backgroundColor = person.eye_color
+    let nameElement = document.createElement('p')
+    nameElement.className = 'name'
+    nameElement.textContent = person.name
 
-  personElement.appendChild(planetElement)
-  mainContainer.appendChild(personElement)
+    personElement.style.borderColor = person.eye_color
+
+    let planetElement = document.createElement('p')
+    planetElement.className = 'planet'
+    planetElement.textContent = person.home
+
+    let personImage = document.createElement('img')
+    personImage.src = person.personImagePath
+
+    personElement.appendChild(personImage)
+    personElement.appendChild(nameElement)
+    personElement.appendChild(planetElement)
+    mainContainer.appendChild(personElement)
+
+    intro.appendChild(mainContainer)
 })
-
-document.body.appendChild(mainContainer)
